9532577810b0b865f9afa74ff037a927585f889f
JasonYapzx <jasonyapzx@gmail.com>
2022-08-14 19:49:19 +0800

    fix: vercel deployment

--- components/Common/LoadingScreen.js
+++ components/Common/LoadingScreen.js
@@ -15,8 +15,8 @@ export default function LoadingScreen() {
         }`}
       >
             <div className="starwars-demo">
-                <img src="//cssanimation.rocks/demo/starwars/images/star.svg" alt="Star" class="star" />
-                <img src="//cssanimation.rocks/demo/starwars/images/wars.svg" alt="Wars" class="wars" />
+                <img src="//cssanimation.rocks/demo/starwars/images/star.svg" alt="Star" className="star" />
+                <img src="//cssanimation.rocks/demo/starwars/images/wars.svg" alt="Wars" className="wars" />
                 <h2 className="byline" id="byline">The Force Awakens</h2>
             </div>
         </div>
